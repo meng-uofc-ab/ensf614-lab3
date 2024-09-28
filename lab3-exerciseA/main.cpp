fc8c37792743a4da98ddcd74dfede06f5f8832d6
Abhilash Paul <paul.abhilash@hotmail.com>
2024-09-28 11:35:13 -0600

    Initial Commit

---
+++ lab3-exerciseA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab3-exerciseA
+//
+//  Created by Abhilash Paul on 2024-09-28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
