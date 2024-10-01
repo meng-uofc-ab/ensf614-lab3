a12cfdd68132d2f5bb788d2ff39276d136e30a07
Abhilash Paul <paul.abhilash@hotmail.com>
2024-10-01 14:36:47 -0600

    clean up iterator

--- lab3-exerciseB/iterator.cpp
+++ lab3-exerciseB/iterator.cpp
@@ -25,111 +25,129 @@ public:
     public:
         VectIter(Vector& x): v(&x), index(0){};
         
-        T operator++()
+        T operator++();
         //PROMISES: increments the iterator's indes and return the
         //          value of the element at the index position. If
         //          index exceeds the size of the array it will
         //          be set to zero. Which means it will be circulated
         //          back to the first element of the vector.
-        {
-            if (++index >= v->size) index = 0;
-            return v->array[index];
-        }
         
-        T operator++(int)
+        T operator++(int);
         // PRIMISES: returns the value of the element at the index
         //           position, then increments the index. If
         //           index exceeds the size of the array it will
         //           be set to zero. Which means it will be circulated
         //           back to the first element of the vector.
-        {
-            T temp = v->array[index];
-            if (++index >= v->size) index = 0;
-            return temp;
-        }
         
-        T operator--()
+        T operator--();
         // PROMISES: decrements the iterator index, and return the
         //           the value of the element at the index. If
         //           index is less than zero it will be set to the
         //           last element in the aray. Which means it will be
         //           circulated to the last element of the vector.
-        {
-            if (--index < 0) index = v->size - 1;
-            return v->array[index];
-        }
         
-        T operator--(int)
+        T operator--(int);
         // PRIMISES: returns the value of the element at the index
         //           position, then decrements the index. If
         //           index is less than zero it will be set to the
         //           last element in the aray. Which means it will be
         //           circulated to the last element of the vector.
-        {
-            T temp = v->array[index];
-            if (--index < 0) index = v->size - 1;
-            return temp;
-        }
         
-        T operator *()
-        // PRIMISES: returns the value of the element at the current
-        //           index position.
-        {
-            return v->array[index];
-        }
+        T operator *();
+        // PRIMISES: returns the value of the element at the current index position.
     };
 
     Vector(int sz): size(sz), array(new T[sz]) {}
-    ~Vector(){ delete[] array; }
-    
-    //copy constructor
-    Vector(const Vector& other) : size(other.size), array(new T[other.size]) {
-        for (int i = 0; i < size; ++i) {
-            array[i] = other.array[i];
-        }
-    }
-    
-    // assignment operator
-    Vector& operator=(const Vector& other) {
-        if (this != &other) {
-            delete[] array;
-            size = other.size;
-            array = new T[size];
-            for (int i = 0; i < size; ++i) {
-                array[i] = other.array[i];
-            }
-        }
-        return *this;
-    }
+    ~Vector();                                      // destructor
+    Vector(const Vector& other);                    //copy constructor
+    Vector<T>& operator=(const Vector& other);      //assignment operator
 
-    T& operator[](int i)
-    // PRIMISES: returns existing value in the ith element of
-    //           array or sets a new value to  the ith element in
-    //           array.
-    { 
-        return array[i];
-    }
+    T& operator[](int i);
+    // PRIMISES: returns existing value in the ith element of array or sets a new value to  the ith element in array.
   
-    void ascending_sort()
+    void ascending_sort();
     // PRIMISES: sorts the vector values in ascending order.
-    {
-        for(int i=0; i< size-1; i++)
-            for(int j=i+1; j < size; j++)
-                if(array[i] > array[j])
-                    swap(array[i], array[j]);
-    }
     
 private:
     T *array;               // points to the first element of an array of T
     int size;               // size of array
-    void swap(T& a, T& b)
+    void swap(T& a, T& b);
     // PRIMISES: swaps the values of two elements in array
-    {
-        T tmp = a;
-        a = b;
-        b = tmp;
-    }
 };
+
+template <typename T>
+T Vector<T>::VectIter::operator++() {
+    if (++index >= v->size) index = 0;
+    return v->array[index];
+}
+
+template <typename T>
+T Vector<T>::VectIter::operator++(int){
+    T temp = v->array[index];
+    if (++index >= v->size) index = 0;
+    return temp;
+}
+
+template <typename T>
+T Vector<T>::VectIter::operator--(){
+    if (--index < 0) index = v->size - 1;
+    return v->array[index];
+}
+
+template <typename T>
+T Vector<T>::VectIter::operator--(int){
+    T temp = v->array[index];
+    if (--index < 0) index = v->size - 1;
+    return temp;
+}
+
+template <typename T>
+T Vector<T>::VectIter::operator *() {
+    return v->array[index];
+}
+
+template <typename T>
+Vector<T>::~Vector(){ delete[] array; }
+
+template <typename T>
+Vector<T>::Vector(const Vector& other) : size(other.size), array(new T[other.size]) {
+    for (int i = 0; i < size; ++i) {
+        array[i] = other.array[i];
+    }
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector& other) {
+    if (this != &other) {
+        delete[] array;
+        size = other.size;
+        array = new T[size];
+        for (int i = 0; i < size; ++i) {
+            array[i] = other.array[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+T& Vector<T>::operator[](int i) {
+    return array[i];
+}
+
+template <typename T>
+void Vector<T>::ascending_sort() {
+    for(int i=0; i< size-1; i++)
+        for(int j=i+1; j < size; j++)
+            if(array[i] > array[j])
+                swap(array[i], array[j]);
+}
+
+template <typename T>
+void Vector<T>::swap(T& a, T& b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
 
 // Specialization for const char* to handle string comparisons correctly
 template <>
